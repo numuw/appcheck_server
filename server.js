52f67aa2720805db965ed5063a50cc783c439dfb
HumedMohammed <humed@numuw.com>
2025-10-26 00:16:22 +0300

    fix: update CORS middleware to use environment variable for origin

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 app.set("trust proxy", true);
 // Middleware
-app.use(cors());
+app.use(cors({ origin: process.env.CORS_ORIGIN }));
 app.use(morgan("dev"));
 
 // Log all requests
